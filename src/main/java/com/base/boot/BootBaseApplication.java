4dc1dfbcfb98bb793fa29e90c9ecb61f84f25459
minki-jeon <minki.jeon.study@gmail.com>
2024-11-22 19:46:19 +0900

    first commit, Create Spring Boot Project

---
+++ src/main/java/com/base/boot/BootBaseApplication.java
@@ -0,0 +1,13 @@
+package com.base.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/base/boot/BootBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
